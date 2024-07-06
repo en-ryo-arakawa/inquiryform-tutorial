c6fd30b55d42c1a8965e2f181f7cec8e3f0c17e0
ryo_arakawa@en-japan.com <ryo_arakawa@en-japan.com>
2024-07-06 12:33:53 +0900

    add: first commit

---
+++ components/MailForm/MailForm.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import React from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { useForm } from "react-hook-form";
+
+const MailForm = () => {
+  const form = useForm();
+
+  function onSubmit(values: any) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={() => {
+          form.handleSubmit(onSubmit);
+        }}
+        className="container flex flex-col gap-3"
+      >
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default MailForm;
